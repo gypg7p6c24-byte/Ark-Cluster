40a2c4129d4d8ceba0086f25426d1b367bff3b5c
gypg7p6c24-byte <gypg7p6c24@privaterelay.appleid.com>
2026-01-12 23:24:08 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,19 @@ RCON_PORT=${RCON_PORT}
 CLUSTER_ID=${CLUSTER_ID}
 ADMIN_PASSWORD=${ADMIN_PASSWORD}
 
+mkdir -p /home/steam/.steam
+mkdir -p /home/steam/.steam/steam
+mkdir -p /home/steam/.steam/root
+
 echo "▶ Backup de sécurité avant update"
-/backup.sh || true
+sh /backup.sh || true
 
 echo "▶ Installing / Updating ARK (${MAP})"
-steamcmd +login anonymous \
- +force_install_dir /ark \
- +app_update 376030 validate \
- +quit
+steamcmd \
+  +force_install_dir /ark \
+  +login anonymous \
+  +app_update 376030 validate \
+  +quit
 
 # Backup toutes les 6h
 (crontab -l 2>/dev/null; echo "0 */6 * * * /backup.sh") | crontab -
