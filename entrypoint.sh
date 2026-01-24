8c5f0b6c4049e03b229e37a8182c530e6c342261
gypg7p6c24-byte <gypg7p6c24@privaterelay.appleid.com>
2026-01-24 14:05:17 +0100

    Comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,14 +20,15 @@ echo "[ARK] Checking server installation..."
 
 if [ ! -f "$SERVER_BIN" ]; then
   echo "[ARK] Installing via SteamCMD..."
-
+echo "[ARK] Create Server repository"
   mkdir -p "$ARK_DIR"
-
+echo "[ARK] Repository created"
   "$STEAMCMD" \
     +force_install_dir "$ARK_DIR" \
     +login anonymous \
     +app_update 376030 validate \
     +quit
+echo "[ARK] Server installed"
 else
   echo "[ARK] Server already installed"
 fi
@@ -35,19 +36,20 @@ fi
 
 SAVE_DIR="${ARK_DIR}/ShooterGame/Saved"
 CLUSTER_DIR="${SAVE_DIR}/clusters/"
-
+echo "[ARK] Creating cluster repository"
 mkdir -p "${CLUSTER_DIR}"
-
+echo "[ARK] Cluster repository created"
+echo "[ARK] Initializing arguments server"
 # Server Launching Script
 ARGS=(
   "${SERVER_MAP}?listen?SessionName=${SESSION_NAME}?ServerPassword=${SERVER_PASSWORD}?ServerAdminPassword=${ADMIN_PASSWORD}?MaxPlayers=${MAX_PLAYERS}"
 )
-
+echo "[ARK] Map, session, password, adm password and max player configurated"
 # Mods (optional)
 if [ -n "${GAME_MOD_IDS}" ]; then
   ARGS+=("?GameModIds=${GAME_MOD_IDS}")
 fi
-
+echo "[ARK] Logs configurated"
 # Logs
 ARGS+=(
   "-server"
@@ -55,7 +57,7 @@ ARGS+=(
   "-servergamelog"
   "-servergamelogincludetribelogs"
 )
-
+echo "[ARK] Network configurated"
 # Network
 ARGS+=(
   "-Port=${GAME_PORT}"
@@ -68,5 +70,5 @@ ARGS+=(
 echo "[ARK] Starting server..."
 "${SERVER_BIN}" "${ARGS[@]}" &
 ARK_PID=$!
-
+echo "[ARK] Server Up"
 wait "$ARK_PID"
